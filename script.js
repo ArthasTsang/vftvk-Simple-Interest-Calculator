a408c4bb085e70f0ee444c9041341b007a5a1a65
Arthas Tsang <twyarthas@yahoo.com.hk>
2022-04-29 23:38:58 +0800

    modified scripts

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+	var year = document.getElementById("years").value;
+	var interest= principal * rate/100 * year ;
+	var curYear= (new Date()).getFullYear();
+	var endYear= curYear+parseInt(year);
+	
+	var result= document.getElementById("result");
+	var resultTxt= "If you deposit "+principal+"<br>";
+	resultTxt= resultTxt+"at an interest rate of "+rate+"%.<br>";
+	resultTxt= resultTxt+"You will receive an amount of "+interest+",<br>";
+	resultTxt= resultTxt+"in the year "+endYear+"<br>";
+	result.innerHTML= resultTxt;
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
         
